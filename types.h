45add66e383bb97b7b0fcfd47a745dd84e762164
Artem <artem@Artems-MacBook-Pro.local>
2015-04-16 12:30:21 +0300

    Offset fixed. Districts added

--- types.h
+++ types.h
@@ -6,7 +6,7 @@ enum Commands {PLAN_BUZ = 37, PLAN_SUZ, CHANGE_TO, CACHE_OUT, LOAD_REQUEST_DIKON
 //should wait for respond to offset Stream from Client or just do it
 #define WAIT_FOR_RESPOND
 //prevents Streams to be planned without offset (graph.cpp)
-#define TEST_MOVE_STREAM
+//#define TEST_MOVE_STREAM
 //local testing (need to fix after Client<-->Server implementation) (testplan.cpp)
 //#define TEST
 
